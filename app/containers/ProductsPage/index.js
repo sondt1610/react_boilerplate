3f8c90986ca8950e085c24d2ff6a1cac89539aea
sondt6951 <sondt6951@co-well.com.vn>
2020-08-21 18:20:11 +0700

    adfghdf

--- app/containers/ProductsPage/index.js
+++ app/containers/ProductsPage/index.js
@@ -2,7 +2,7 @@
   W00 Products Page
 */
 
-import React, { useEffect, memo } from 'react';
+import React, { useState, useEffect, memo } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
@@ -11,14 +11,23 @@ import { useInjectReducer } from 'utils/injectReducer';
 import { useInjectSaga } from 'utils/injectSaga';
 import Breadcrumb from 'components/Breadcrumb';
 import { Container } from 'components/Container';
+import { Checkbox, Radio } from 'antd';
 import reducer from './reducer';
 import saga from './saga';
 import * as TYPE from './actionTypes';
+
 const key = 'products';
 
 export function ProductsPage(props) {
   useInjectReducer({ key, reducer });
   useInjectSaga({ key, saga });
+
+  const [value, setValue] = useState(1);
+
+  const onChange = () => {};
+  const onChangeRadio = e => {
+    setValue(e.target.value);
+  };
 
   useEffect(() => {
     // Call api
@@ -29,7 +38,17 @@ export function ProductsPage(props) {
     // <Container>Product List</Container>
     <>
       <Breadcrumb />
-      <Container className="site-layout-background">Product List</Container>
+      <Container className="site-layout-background">
+        <div>Product List</div>
+        <Checkbox onChange={onChange}>Buy</Checkbox>
+        <div>Size</div>
+        <Radio.Group onChange={onChangeRadio} value={value}>
+          <Radio value={1}>S</Radio>
+          <Radio value={2}>XS</Radio>
+          <Radio value={3}>L</Radio>
+          <Radio value={4}>M</Radio>
+        </Radio.Group>
+      </Container>
     </>
   );
 }
